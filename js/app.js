652a1070ca2f8ec0e2a95617b9a8e3cbbb630dca
Shmelkow <sol2390@gmail.com>
2020-01-12 22:55:58 +0200

    header

---
+++ js/app.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+   /*  $(".ba-menu-btn").on("click", function() {
+        $(".ba-menu").toggle();
+    }); */
+
+    $(".ba-menu-btn").on("click", function() {
+        $(".ba-menu").toggleClass("open");
+    });
+
+    /* $(window).on('resize', function() {
+        
+        if ( $(this).width() > 991 ) {
+            $(".ba-menu").show();
+        } else {
+            $(".ba-menu").hide();
+
+        }
+    }); */
+    
+    $("#showSearch").on("click", function() {
+        $(".ba-search-form").toggleClass("open");
+        $("#showSearch i").toggleClass("fa-search fa-times");
+        
+        //$(this).children().toggleClass("fa-search fa-times");
+        
+        /* if ($(".ba-search-form").hasClass("open")) {
+            $(this).children().toggleClass("fa-search fa-times")
+        } else {
+            $(this).children().toggleClass("fa-times fa-search")
+        } */
+    });
+    
+
+})
